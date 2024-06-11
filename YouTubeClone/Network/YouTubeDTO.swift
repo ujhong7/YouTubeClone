db807b9787813c093e538e0ad3bd71a66b5b96ab
ujhong7 <ujhong7>
2024-06-11 16:29:33 +0900

    데이터 설명 추가

--- YouTubeClone/Network/YouTubeDTO.swift
+++ YouTubeClone/Network/YouTubeDTO.swift
@@ -23,24 +23,40 @@ struct Item: Codable {
 
 // MARK: - Snippet
 struct Snippet: Codable {
+    
+    /// 동영상이 게시된 날짜
     let publishedAt: String // Date -> String
-    let channelId : String // 채널아이디 (채널 프로필이미지 구하기 위함)
+    
+    /// 채널 고유 식별 ID
+    let channelId : String
+    
+    /// 동영상 제목
     let title: String
+    
     let thumbnails: Thumbnails
-    let channelTitle: String // 채널제목
+    
+    /// 동영상이 속한 채널의 채널 제목
+    let channelTitle: String
 }
 
 // MARK: - Thumbnails
 struct Thumbnails: Codable {
+    /// 썸네일 이미지의 고해상도 버전
     let medium: ThumbnailDetail
 }
 
 // MARK: - ThumbnailDetail
 struct ThumbnailDetail: Codable {
+    /// 이미지의 URL
     let url: String
 }
 
 // MARK: - Statistics
 struct Statistics: Codable {
-    let viewCount, likeCount, favoriteCount, commentCount: String
+    /// 조회수
+    let viewCount: String
+    /// 좋아요 수
+    let likeCount: String
+    /// 댓글 수
+    let commentCount: String
 }
